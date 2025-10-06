fd277913b0594dea9d37f2cfb357a0599142cbb5
shaun macdougall <me@shaunmacdougall.com>
2025-10-07 10:18:33 +1100

    add email icon to pre load stack

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -7,10 +7,10 @@ import { library } from '@fortawesome/fontawesome-svg-core';
 /* import font awesome icon component */
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
-import { faLifeRing, faHandshake, faRuler, faCodeBranch, faMicrochip, faCalculator, faPeoplePulling, faRoad } from "@fortawesome/free-solid-svg-icons";
+import { faLifeRing, faHandshake, faRuler, faCodeBranch, faMicrochip, faCalculator, faPeoplePulling, faRoad, faEnvelope  } from "@fortawesome/free-solid-svg-icons";
 
 import { faGithub, faCodepen, faXTwitter, faSquareJs, faCss3, faReact } from "@fortawesome/free-brands-svg-icons";
 
-library.add( faLifeRing, faHandshake, faRuler, faCodeBranch, faMicrochip, faCalculator, faPeoplePulling, faRoad, faGithub, faCodepen, faXTwitter, faSquareJs, faCss3, faReact);
+library.add( faLifeRing, faHandshake, faRuler, faCodeBranch, faMicrochip, faCalculator, faPeoplePulling, faRoad, faGithub, faCodepen, faXTwitter, faSquareJs, faCss3, faReact, faEnvelope);
 
 createApp(App).component('font-awesome-icon', FontAwesomeIcon).mount('#app');
