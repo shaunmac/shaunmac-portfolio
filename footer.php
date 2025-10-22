a5ab1a5ac80643c64ff2b99db9c91bd38f6c344b
shaun macdougall <me@shaunmacdougall.com>
2025-10-22 15:26:21 +1100

    deploy

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
-<script type="module" src="<?php echo get_template_directory_uri(); ?>/frontend/dist/assets/index.51b490da.js"></script>
+<script type="module" src="<?php echo get_template_directory_uri(); ?>/frontend/dist/assets/index.846f2a9a.js"></script>
 <?php wp_footer(); ?>
 
 </body>
--- header.php
+++ header.php
@@ -7,7 +7,7 @@
       charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="profile" href="http://gmpg.org/xfn/11">
-    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/frontend/dist/assets/index.d4685bcc.css">
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/frontend/dist/assets/index.66516b3c.css">
     <?php wp_head(); ?>
   </head>
 
